a280d42386bfdddd0ea5b1255b8d82fcdabb3368
Merril Jeffs <merril@Merrils-MacBook-Pro.local>
2017-03-08 10:30:53 -0700

    styling pages

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,18 @@
-// $( document ).ready(function() {
-//   $('#myCollapsible').collapse({
-//     toggle: false
-//   })
-// })
+$(document).ready(function(){
+
+  // Input form validation
+  var $text = $('input[type="text"]');
+  var $textarea = $('textarea');
+  var $submit = $('input[type="submit"]');
+
+  $submit.prop('disabled', true);
+  $text.on('keyup', checkStatus);
+  $textarea.on('keyup', checkStatus);
+
+
+function checkStatus() {
+    var status = ($.trim($text.val()) === '' || $.trim($textarea.val()) === '');
+    $submit.prop('disabled', status);
+}
+
+})
